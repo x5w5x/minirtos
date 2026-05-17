04e987c57160896a592c74ff6f8f41eff7f11500
慕芸婷 <12344332+xx3457388139@user.noreply.gitee.com>
2026-05-17 11:41:02 +0800

    初始化仓库

---
+++ User/main.c
@@ -0,0 +1,23 @@
+/*
+ * @Author: 轩
+ * @Date: 2026-02-06 19:12:36
+ * @LastEditTime: 2026-05-17 11:36:49
+ * @FilePath: \minirtos\User\main.c
+ */
+
+#include "stm32f10x.h"                  // Device header
+
+
+int main(void)
+{
+
+	
+
+	while (1)
+	{
+		
+
+	}
+}
+
+
